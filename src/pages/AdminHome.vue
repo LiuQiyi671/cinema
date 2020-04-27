dfc75a5042c1e39e908c539900d004b7a23a7dc0
LiuQiyi671 <1271359259@qq.com>
2020-04-28 01:01:54 +0800

    <4.28>

--- src/pages/AdminHome.vue
+++ src/pages/AdminHome.vue
@@ -1,46 +1,49 @@
 <template>
     <div id="app">
         <div id="home">
-<!--            导航头header部分-->
+            <!--            导航头header部分-->
             <el-row class="header">
                 <div class="head-bar">
                     <img src="../assets/img/logo.png" height="50" width="50">
                     <span slot="title" style="padding-left:25px;font-size: 40px;font-weight: bolder;">UPC Online</span>
+
+                    <el-button style="position: absolute; margin-left: 1250px; z-index: 999" @click="adminLogout" type="info"
+                               circle>退出
+                    </el-button>
+
                 </div>
             </el-row>
 
-<!--            导航栏部分-->
+            <!--            导航栏部分-->
             <el-row class="container">
-                <div class="section">
-                    <div class="nav-bar">
-                        <el-menu :default-active="currentMenuIndex" class="el-menu-vertical">
+                <div class="nav-bar">
+                    <el-menu :default-active="currentMenuIndex" class="el-menu-vertical">
 
-                            <el-menu-item index="/user_manage" @click="handleMenuItemClick('/user_manage')">
-                                <i class="el-icon-user-solid"></i>
-                                <span slot="title">用户管理</span>
-                            </el-menu-item>
+                        <el-menu-item index="/user_manage" @click.native="handleMenuItemClick('/user_manage')">
+                            <i class="el-icon-user-solid"></i>
+                            <span slot="title">用户管理</span>
+                        </el-menu-item>
 
-                            <el-menu-item index="/movie_manage" @click="handleMenuItemClick('/movie_manage')">
-                                <i class="el-icon-video-camera-solid"></i>
-                                <span slot="title">电影管理</span>
-                            </el-menu-item>
+                        <el-menu-item index="/movie_manage" @click.native="handleMenuItemClick('/movie_manage')">
+                            <i class="el-icon-video-camera-solid"></i>
+                            <span slot="title">电影管理</span>
+                        </el-menu-item>
 
-                            <el-menu-item index="/movie_schedule" @click="handleMenuItemClick('/schedule_manage')">
-                                <i class="el-icon-s-fold"></i>
-                                <span slot="title">排片管理</span>
-                            </el-menu-item>
+                        <el-menu-item index="/movie_schedule" @click.native="handleMenuItemClick('/schedule_manage')">
+                            <i class="el-icon-s-fold"></i>
+                            <span slot="title">排片管理</span>
+                        </el-menu-item>
 
-                            <el-menu-item index="/news_manage" @click="handleMenuItemClick('/news_manage')">
-                                <i class="el-icon-s-comment"></i>
-                                <span slot="title">资讯管理</span>
-                            </el-menu-item>
+                        <el-menu-item index="/news_manage" @click.native="handleMenuItemClick('/news_manage')">
+                            <i class="el-icon-s-comment"></i>
+                            <span slot="title">资讯管理</span>
+                        </el-menu-item>
 
-                            <el-menu-item index="/order_manage" @click="handleMenuItemClick('/order_manage')">
-                                <i class="el-icon-s-ticket"></i>
-                                <span slot="title">订单管理</span>
-                            </el-menu-item>
-                        </el-menu>
-                    </div>
+                        <el-menu-item index="/order_manage" @click.native="handleMenuItemClick('/order_manage')">
+                            <i class="el-icon-s-ticket"></i>
+                            <span slot="title">订单管理</span>
+                        </el-menu-item>
+                    </el-menu>
                 </div>
             </el-row>
         </div>
@@ -52,18 +55,25 @@
         name: "AdminHome",
         data() {
             return {
+
                 // 当前所在导航栏
-                currentMenuIndex: '/user_manage'
+                currentMenuIndex: '/user_manage',
             };
         },
         created() {
 
         },
         methods: {
+
             // 导航栏跳转
             handleMenuItemClick(path) {
                 this.$router.push('/admin' + path);
                 this.currentMenuIndex = path;
+            },
+
+            // 管理员退出系统
+            adminLogout() {
+                this.$router.push('/admin/login');
             }
         }
     };
@@ -77,45 +87,16 @@
     }
 
     .container {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-        min-width: 1200px;
-        background-color: #f5f5f5;
-    }
-
-    .container .section {
-        width: 80%;
-        min-width: 1200px;
-        display: flex;
-        justify-content: right;
-        align-content: flex-start;
-        height: 100%;
-        box-sizing: border-box;
+        position: absolute;
         margin-top: 30px;
+        margin-left: 160px;
     }
 
     .nav-bar {
-        width: 10%;
-        min-width: 200px;
+        min-width: 140px;
         border-bottom: 1px solid #e9eaeb;
     }
 
-    .container .content {
-        display: flex;
-        justify-content: center;
-        align-content: flex-start;
-        width: 90%;
-        min-width: 1000px;
-        height: 720px;
-        padding-top: 30px;
-        background-color: #fff;
-        border: 1px solid #e9eaeb;
-        border-left: none;
-        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .05);
-    }
 </style>
 <style scoped lang="stylus" ref="stylesheet/stylus">
     #app
@@ -148,28 +129,13 @@
         min-width: 1200px;
     }
 
-    /*#home .el-dropdown-link {*/
-    /*    cursor: pointer;*/
-    /*    font-size: 14px;*/
-    /*}*/
-    /*#home .el-dropdown-link:hover {*/
-    /*    color: #409eff;*/
-    /*}*/
-    /*#home .el-icon-arrow-down {*/
-    /*    font-size: 12px;*/
-    /*}*/
     #home .el-menu {
         border: none;
         border: 1px solid #e9eaeb;
         box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .05);
     }
 
-    #home .el-menu-item {
-        height: 50px;
-        line-height: 50px;
-    }
-
-    #home .el-menu-item, #home .el-menu-item i {
+    #home .el-menu-item i {
         color: #3c4043;
     }
 
